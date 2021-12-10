f9e9996b299ef5829afb4729d1e53531f637cafa
patricey0 <patrice.yolle@gmail.com>
2021-12-10 11:52:43 +0100

    orga et mcd ok

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const router = require('./app/router');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use('/v1', router)
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
